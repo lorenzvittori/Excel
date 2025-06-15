df15d64855b3f9a07a396e8b90994e5a85d8a363
lorenzvittori <lorenzvittori@gmail.com>
2025-06-15 16:34:25 +0200

    gestione directory

--- main.py
+++ main.py
@@ -1,122 +1,87 @@
 import pandas as pd
 
+# ------------------------------ CONFIGURAZIONE ------------------------------
+
 MeseAttuale = 'Maggio'
 AnnoAttuale = '2025'
+
+
 
 AnnoAttualeShort = AnnoAttuale[2:]
 
 DizMesi = {
-    "Gennaio": "01",
-    "Febbraio": "02",
-    "Marzo": "03",
+    "Gennaio": "01", 
+    "Febbraio": "02", 
+    "Marzo": "03", 
     "Aprile": "04",
-    "Maggio": "05",
-    "Giugno": "06",
-    "Luglio": "07",
+    "Maggio": "05", 
+    "Giugno": "06", 
+    "Luglio": "07", 
     "Agosto": "08",
-    "Settembre": "09",
-    "Ottobre": "10",
-    "Novembre": "11",
+    "Settembre": "09", 
+    "Ottobre": "10", 
+    "Novembre": "11", 
     "Dicembre": "12"
 }
 
-
-# ------------------------------ IMPORT ------------------------------
-
-# File di import
-input_file = r'C:\Users\lvitt\OneDrive\Desktop\Personale\SPESE-ENTRATE' + '\\' + AnnoAttuale + '\\' + MeseAttuale + ' ' + AnnoAttualeShort + ' - App' + '.xlsx'
-
-# Creazione DataFrame di manipolazione
-df = pd.read_excel(input_file, sheet_name='Spese',skiprows=1, header = 0)
-
-# ------------------------------ MANIPOLAZIONE ------------------------------
-
-# Rimozione Colonne inutili
-colonne_da_rimuovere = [2, 3, 4, 6, 7, 8, 9]  # Indici delle colonne da rimuovere
-df.drop(df.columns[colonne_da_rimuovere], axis=1, inplace=True)
-
-# Rinomina dell'intestazione
-df.rename(columns={df.columns[0]: 'Data'}, inplace=True)
-df.rename(columns={df.columns[1]: 'Categoria'}, inplace=True)
-df.rename(columns={df.columns[2]: 'Importo'}, inplace=True)
-df.rename(columns={df.columns[3]: 'Commento'}, inplace=True)
+# Percorsi dei file
+folder_path = r'C:\Users\lvitt\OneDrive\Desktop\Personale\SPESE-ENTRATE'
 
 
-# ------------------------------ RIFORMATIZZAZIONE ------------------------------
-
-# Cambia il formato della colonna "Data"
-df['Data'] = pd.to_datetime(df['Data'], errors='coerce')
-# Riformatta la colonna "Data" nel formato dd/mm/yyyy
-df['Data'] = df['Data'].dt.strftime('%d/%m/%Y')
+default_input = fr'{folder_path}\{AnnoAttuale}\{MeseAttuale} {AnnoAttualeShort} - App.xlsx'
+default_output = fr'{folder_path}\{AnnoAttuale}\{MeseAttuale} {AnnoAttualeShort} - Processed.xlsx'
 
 
+# Opzionali input/output personalizzati
+customize_input = r''
+customize_output = r''
 
-# ------------------------------ RIGHE STANDARD ------------------------------
-# ------------------------------ FUNZIONI ------------------------------
-def genera_data_formattata(giorno: str) -> str:
+input_file = default_input if customize_input == '' else customize_input
+output_file = default_output if customize_output == '' else customize_output
+
+# ------------------------------ FUNZIONI UTILI ------------------------------
+
+def DAY_TO_DATA(giorno: str) -> str:
     return f"{giorno.zfill(2)}/{DizMesi[MeseAttuale]}/{AnnoAttuale}"
 
+# ------------------------------ LETTURA FILE PRINCIPALE ------------------------------
 
-# ------------------------------ RIGHE STANDARD ------------------------------
-nuove_righe_array = [
-    {
-        'GiornoData': '04',
-        'Categoria': 'Commissione CC',
-        'Importo': 1.6
-    },
-    {
-        'GiornoData': '05',
-        'Categoria': 'Rata PDR',
-        'Importo': 50
-    },
-    {
-        'GiornoData': '11',
-        'Categoria': 'Abbonamento Telefono',
-        'Importo': 7.99
-    },
-    {
-        'GiornoData': '20',
-        'Categoria': 'Spotify',
-        'Importo': 3
-    },
-]
+df = pd.read_excel(input_file, sheet_name='Spese', skiprows=1, header=0)
 
-# Genera il DataFrame da nuove_righe_array
-nuove_righe = pd.DataFrame([
-    {
-        'Data': genera_data_formattata(riga['GiornoData']),
-        'Categoria': riga['Categoria'],
-        'Importo': riga['Importo'],
-        'Commento': ''
-    }
-    for riga in nuove_righe_array
-])
+# Rimuovo colonne inutili
+colonne_da_rimuovere = [2, 3, 4, 6, 7, 8, 9]
+df.drop(df.columns[colonne_da_rimuovere], axis=1, inplace=True)
 
-# Unione al DataFrame principale
+# Rinomino intestazioni
+df.rename(columns={
+    df.columns[0]: 'Data',
+    df.columns[1]: 'Categoria',
+    df.columns[2]: 'Importo',
+    df.columns[3]: 'Commento'
+}, inplace=True)
+
+# Formattazione data
+df['Data'] = pd.to_datetime(df['Data'], errors='coerce')
+df['Data'] = df['Data'].dt.strftime('%d/%m/%Y')
+
+# ------------------------------ LETTURA RIGHE AGGIUNTIVE DA CSV ------------------------------
+
+df_nuove_righe_raw = pd.read_csv('added_rows.csv')
+
+# Genera la colonna "Data" formattata da "GiornoData"
+df_nuove_righe_raw['Data'] = df_nuove_righe_raw['GiornoData'].apply(DAY_TO_DATA)
+
+# Preparo il DataFrame nello stesso formato
+nuove_righe = df_nuove_righe_raw[['Data', 'Categoria', 'Importo']].copy()
+nuove_righe['Commento'] = ''
+
+# ------------------------------ UNIONE E PULIZIA ------------------------------
+
 df = pd.concat([df, nuove_righe], ignore_index=True)
+df.insert(1, 'Gruppo', '')                # Aggiunge colonna "Gruppo" vuota
+df.sort_values(by='Data', inplace=True)  # Ordina per Data
+df.drop_duplicates(inplace=True)         # Rimuove duplicati
 
-
-# ------------------------------ MANIPOLAZIONE ------------------------------
-
-# Aggiungo la colonna Gruppo vuota
-df.insert(1, 'Gruppo', '')
-
-# Ordino i dati per data
-df.sort_values(by='Data', inplace=True)
-
-# Rimuovi eventuali duplicati
-df.drop_duplicates(inplace=True)
-
-
-
-# ------------------------------ EXPORT ------------------------------
-
-output_file = r'C:\Users\lvitt\OneDrive\Desktop\Personale\SPESE-ENTRATE' + '\\' + AnnoAttuale + '\\' + MeseAttuale + ' ' + AnnoAttualeShort + ' - Processed' + '.xlsx'
+# ------------------------------ ESPORTAZIONE ------------------------------
 
 df.to_excel(output_file, index=False)
-
-
-
-
-
-
