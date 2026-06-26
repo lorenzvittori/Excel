e5f7b1c20166f8ede0da0f8279f979ea2ff23831
LorenzoVittori <lorenzvittori@gmail.com>
2026-06-26 17:20:36 +0200

    download da dropbox e salvataggio in cartella locale

--- TEST_TOKEN.py
+++
@@ -1,5 +0,0 @@
-import dropbox
-
-dbx = dropbox.Dropbox("IL_TUO_TOKEN")
-
-print(dbx.users_get_current_account())
--- download_from_dropbox.py
+++ download_from_dropbox.py
@@ -1,28 +1,30 @@
 import dropbox
-import os
+from pathlib import Path
 
-# -----------------------
-# CONFIG
-# -----------------------
-DROPBOX_TOKEN = os.environ["DROPBOX_TOKEN"]
+from MAIN import FILEAPP_FOLDER
+
+#Constanti
+TOKEN_FILE = "token_dropbox.txt"
+MAIN_FOLDER = "Dati"
+FILE_APP_FOLDER = "TabelleApp"
 
 FILE_NAME = "app_2026_06.xlsx"
-DROPBOX_PATH = f"/Finanze/{FILE_NAME}"   # cambia cartella se serve
-LOCAL_PATH = f"data/raw/{FILE_NAME}"
 
-# -----------------------
-# CLIENT
-# -----------------------
-dbx = dropbox.Dropbox(DROPBOX_TOKEN)
 
-# -----------------------
-# DOWNLOAD
-# -----------------------
-os.makedirs("data/raw", exist_ok=True)
+BASE_DIR = Path(__file__).resolve().parent
+TOKEN_FILE_DIR = BASE_DIR / TOKEN_FILE
 
-try:
-    dbx.files_download_to_file(LOCAL_PATH, DROPBOX_PATH)
-    print(f"Download completato: {LOCAL_PATH}")
 
-except Exception as e:
-    print("Errore download:", e)
+# Token
+TOKEN = TOKEN_FILE_DIR.read_text().strip()
+dbx = dropbox.Dropbox(TOKEN)
+
+
+# file target
+DROPBOX_PATH = f"/{FILE_APP_FOLDER}/{FILE_NAME}"
+LOCAL_PATH = BASE_DIR / MAIN_FOLDER / FILE_APP_FOLDER / FILE_NAME
+
+# download
+dbx.files_download_to_file(str(LOCAL_PATH), DROPBOX_PATH)
+
+print("Download completato:", LOCAL_PATH)
