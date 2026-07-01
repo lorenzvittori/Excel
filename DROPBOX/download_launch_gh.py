a535cb17d392fd3ef613d03e4d8855fb7f8d151e
lorenzvittori <lorenzvittori@gmail.com>
2026-07-01 12:17:56 +0200

    Gestione anno e mese

--- DROPBOX/download_launch_gh.py
+++ DROPBOX/download_launch_gh.py
@@ -1,9 +1,15 @@
 import dropbox_module as dropbox_utils
 from pathlib import Path
 
+
+ANNO = "2026"
+MESE = "06"
+
+file_name = f"app_{ANNO}_{MESE}.xlsx"
+
 if __name__ == "__main__":
     dropbox_utils.download_file_from_dropbox(
         Path("Dati/TabelleApp"),
-        "app_2026_06.xlsx",
+        file_name,
         blocca_se_esistente=True
     )
