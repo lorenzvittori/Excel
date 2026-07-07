5fe8da2fca90e558f17ad76765aee30aa312ebb1
Lorenzo Vittori <146745420+lorenzvittori@users.noreply.github.com>
2026-07-08 00:30:39 +0200

    Update new_logger.py

--- new_logger.py
+++ new_logger.py
@@ -1,1 +1,22 @@
+_contatore_phase = 0
+_profonditá = 0
+
 def new_phase(corpo: str):
+if _contatoee_phase == 0:
+print(f"Fase {_contatore_phase} - {corpo})
+else:
+print(f"- {corpo}")
+_profonditá = _profonditá + 1
+
+
+def end_phase:
+_pronfonditá = max(0, _profonditá -1)
+
+def get_tab(n: int)
+return "\t" * n
+
+
+
+
+
+def end
